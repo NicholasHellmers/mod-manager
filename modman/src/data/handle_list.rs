4d504b3964049dc3a94cf12330db0ac08c05b879
Nicholas Hellmers <nihe1661@colorado.edu>
2023-12-31 17:18:25 -0300

    Added: list mods <game.name> feature

--- modman/src/data/handle_list.rs
+++ modman/src/data/handle_list.rs
@@ -5,6 +5,7 @@ pub fn handle_list(args_list: Vec<String>) -> Result<(), std::io::Error> {
     
     match args_list[0].as_str() {
         "games" => print_game_list(),
+        "mods"  => print_mod_list(&args_list[1]).expect("Invalid argument for list"),
         _ => println!("Invalid argument for list"),
         
     }
@@ -14,9 +15,34 @@ pub fn handle_list(args_list: Vec<String>) -> Result<(), std::io::Error> {
 
 fn print_game_list() {
     let game_list = get_game_list_and_path().unwrap();
-    println!("ID, Game Name, Path");
+    println!("ID, Game, Path");
     for (i, game) in game_list.games.iter().enumerate() {
         println!("{}. {}, {}", i, game.name, game.path);
     }
 }
 
+fn print_mod_list(game: &String) -> Result<(), std::io::Error> {
+    let game_list = get_game_list_and_path().unwrap();
+
+    // turn game_list into a hashmap where the key is the game name and the value is the path
+    let mut game_map: std::collections::HashMap<String, String> = std::collections::HashMap::new();
+    for game in game_list.games {
+        game_map.insert(game.name, game.path);
+    }
+
+    // Get the direcories within the path of the selected game
+    let game_path = game_map.get(game).expect("Invalid game name");
+
+    let paths = std::fs::read_dir(game_path).unwrap();
+
+    // Print the only the directories
+    for path in paths {
+        let path = path.unwrap().path();
+        if path.is_dir() {
+            println!("{:}", path.file_name().unwrap().to_str().unwrap());
+        }
+    }
+
+    Ok(())
+    
+}
--- modman/src/main.rs
+++ modman/src/main.rs
@@ -7,32 +7,12 @@ mod util;
 fn main() -> Result<(), std::io::Error>{
     // Get args passed to the program
     let args: Vec<String> = std::env::args().collect();
-    println!("{:?}", args);
-
     let query = &args[1];
-    println!("Searching for {}", query);
 
     match query.as_str() {
         "list" => handle_list(rm_first_two_entries(args)).expect("Invalid argument for list"),
         _ => println!("Invalid command"),
     }
-
-    // // Read user input
-    // let mut input = String::new();
-    // std::io::stdin().read_line(&mut input).unwrap();
-    // let input: usize = input.trim().parse().unwrap();
-
-    // // Get the direcories within the path of the selected game
-    // let game_path = &game_list.games[input].path;
-    // let paths = std::fs::read_dir(game_path).unwrap();
-
-    // // Print the only the directories
-    // for path in paths {
-    //     let path = path.unwrap().path();
-    //     if path.is_dir() {
-    //         println!("{:}", path.file_name().unwrap().to_str().unwrap());
-    //     }
-    // }
 
     Ok(())
 }
